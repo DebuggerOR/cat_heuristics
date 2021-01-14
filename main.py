7601ba9c4f0e67eb6f6d90e3e38692f11127f369
DebuggerOR <foglerori@gmail.com>
2021-01-14 17:11:19 +0200

    finished

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from skimage.io import imread, imsave, imshow
 from skimage.draw import circle
 from abc import ABC, abstractmethod
 import numpy as np
-import matplotlib.colors as mcolors
-from matplotlib import colors
-import matplotlib.pyplot as plt
-import copy
 
 COLORS = []
 
@@ -19,48 +15,33 @@ class State(ABC):
         return self.value
 
     @abstractmethod
-    def next_state(self):
+    def get_neighbors(self, iter):
+        pass
+
+    @abstractmethod
+    def evaluate(self):
         pass
 
 
 class ImageState(State):
-    def next_state(self):
-        print('*** start next ***')
+    def get_neighbors(self, iter):
         neighbors = []
         rows,cols,_ = self.value.shape
-        rad_range = int(min(rows,cols)/4)
+        radious = int(min(rows,cols)/2) / (np.log(iter + 1))
 
-        for r in range(100,rows-100,100):
-            for c in range(100,cols-100,100):
-                for col in COLORS:
-                    rd = 100
-                    rr, cc = circle(r, c, rd)
-                    val = np.copy(self.value)
-                    val[rr, cc] = col
-                    neighbors.append(val)
+        num_rows, num_cols, _ = img_mat.shape
+        rr = np.random.randint(num_rows)
+        rc = np.random.randint(num_cols)
+        for col in COLORS:
+            c1, c2 = circle(rr, rc, radious, shape=(num_rows,num_cols))
+            val = np.copy(self.value)
+            val[c1, c2] = col
+            neighbors.append(ImageState(val,self.goal_value))
 
-        # for r in range(0,rows,100):
-        #     for c in range(0,cols,100):
-        #         for col in COLORS:
-        #             rd = 100
-        #             rr, cc = circle(r, c, rd)
-        #             val = copy.deepcopy(self.value)
-        #             val[rr, cc] = col
-        #             neighbors.append(val)
-                # for rd in range(int(rad_range/11), rad_range, 10):
+        return neighbors
 
-        min_neighbor = neighbors[0]
-        min_val = np.sum((min_neighbor[:,:,0:3]-self.goal_value[:,:,0:3])**2)
-        for n in neighbors:
-            cur_val = np.sum((n[:,:,0:3]-self.goal_value[:,:,0:3])**2)
-            if cur_val < min_val:
-                min_neighbor = n
-                min_val = cur_val
-
-        imshow(min_neighbor)
-        plt.show()
-
-        self.value = min_neighbor
+    def evaluate(self):
+        return np.sum((self.value[:, :, 0:3] - self.goal_value[:, :, 0:3]) ** 2)
 
 
 class SearchAlgo(ABC):
@@ -79,37 +60,79 @@ class HillClimbing(SearchAlgo):
         current = self.init_state
 
         for i in range(self.max_iters):
-            current.next_state()
+            neighbors = current.get_neighbors(i)
+
+            min_val = current.evaluate()
+            for n in neighbors:
+                cur_val = n.evaluate()
+                if cur_val < min_val:
+                    min_val = cur_val
+                    current = n
+
+            # imshow(min_neighbor)
+            # plt.show()
 
         return current
 
 
 class SimulatedAnnealing(SearchAlgo):
-    pass
+    def run(self):
+        current = self.init_state
+        worst_eval = current.evaluate()
+
+        for i in range(self.max_iters):
+            neighbors = current.get_neighbors(i)
+
+            cur_val = current.evaluate()
+            n = np.random.choice(neighbors)
+            n_val = n.evaluate()
+            if cur_val > n_val:
+                current = n
+            else:
+                p = 1 - (n_val / worst_eval)
+                p = p / (np.log(i + 1))
+                if np.random.random() < p:
+                    current = n
+                else:
+                    i-=1
+
+        return current
 
 
-def create_colors():
-    cols = []
-    #tab_cols = mcolors.TABLEAU_COLORS
-    tab_cols = mcolors.BASE_COLORS
-    for c in tab_cols.values():
-        r,g,b = colors.to_rgb(c)
-        r,g,b = int(255*r),int(255*g),int(255*b)
-        cols.append([r,g,b])
-    return cols
+def create_colors(img_mat, colors_num=8, epsilon=300):
+    colors = []
+    num_rows,num_cols,_ = img_mat.shape
+
+    while len(colors) < colors_num:
+        rr = np.random.randint(num_rows)
+        rc = np.random.randint(num_cols)
+        sample = img_mat[rr][rc]
+        is_new = True
+        for j in range(len(colors)):
+            if np.sum((colors[j][:]-sample[:])**2) < epsilon:
+                is_new = False
+        if is_new:
+            colors.append(sample)
+
+    imshow(np.array([colors]))
+    return colors
 
 
 if __name__ == '__main__':
-    COLORS = create_colors()
-
-    img_mat = imread('cat1.jpg')
+    img_mat = imread('cat.jpg')
+    COLORS = create_colors(img_mat)
     goal_state = ImageState(img_mat, img_mat)
 
     init_mat = np.full(img_mat.shape, 255)
     init_state = ImageState(init_mat, img_mat)
 
-    algo = HillClimbing(init_state, 10)
+    algo = HillClimbing(init_state, 10000)
     fin_state = algo.run()
 
-    imsave('my_cat1.png', fin_state.get_value())
+    imsave('hill_cat.png', fin_state.get_value())
+
+    algo = SimulatedAnnealing(init_state, 10000)
+    fin_state = algo.run()
+
+    imsave('sim_cat.png', fin_state.get_value())
 
